59861ad9c3c69473be7af1da4825e387c116fd92
YXCZ <minjing@mc2050.com>
2016-12-16 14:35:03 +0800

    Initial Commit

---
+++ MJWeiBo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MJWeiBo
+//
+//  Created by YXCZ on 16/12/16.
+//  Copyright © 2016年 林民敬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
